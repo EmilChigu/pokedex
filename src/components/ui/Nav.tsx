a82e1841e1b2c93a5872e684676c210bba4b1180
Emil <emil.chigu@gmail.com>
2022-06-30 20:34:09 +0100

    feat: Simple app navigations bar

---
+++ src/components/ui/Nav.tsx
@@ -0,0 +1,20 @@
+function Nav() {
+  return (
+    <>
+      <nav className="p-2 flex w-screen bg-[#fff] shadow-sm w-3/4 justify-center space-x-3">
+        <div>
+          <img
+            src="https://www.freepnglogos.com/uploads/pokeball-png/pokeball-alexa-style-blog-pokemon-inspired-charmander-daily-8.png"
+            alt="pokeball image"
+            width="25px"
+          />
+        </div>
+        <div className="">
+          <h1 className="text-center font-semibold text-xl">Pokedex</h1>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
